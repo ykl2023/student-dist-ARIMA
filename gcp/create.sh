36ff27fc19ea26d6fedd8ecfcd76d71a762acefd
ykl2023 <156436982+ykl2023@users.noreply.github.com>
2024-04-12 22:14:44 +0100

    Update create.sh

--- gcp/create.sh
+++ gcp/create.sh
@@ -1,14 +1,17 @@
-gsutil cp install.sh gs://ok-spark-bucket/install.sh
+### put the install.sh into the bucket 
+#gsutil cp install.sh gs://ok-spark-bucket/install.sh
+### not necessary as we can put it by hand 
 
-gcloud dataproc clusters create ok-py-cl \
-  --region us-central1 \
-  --master-machine-type n1-standard-4 \
-   --master-boot-disk-size 50 \
-   --num-workers 3 \
-   --worker-machine-type n1-standard-4 \
-   --worker-boot-disk-size 50 \
-   --image-version 1.5-debian10 \
-   --scopes 'https://www.googleapis.com/auth/cloud-platform' \
-   --project pyspark-402405 \
-   --enable-component-gateway \
-   --initialization-actions=gs://ok-spark-bucket/install.sh
+gcloud dataproc clusters create cluster-st446-project-ykl \
+  --region europe-west2 \
+  --zone europe-west2-c \
+  --master-machine-type n2-standard-2 \
+  --master-boot-disk-size 500 \
+  --num-workers 3 \
+  --worker-machine-type n2-standard-2 \
+  --worker-boot-disk-size 500 \
+  --image-version 1.5-debian10 \
+  --initialization-actions gs://st446-project-ykl-1204/install.sh \
+  --optional-components ANACONDA,JUPYTER \
+  --enable-component-gateway \
+  --project st446-19012024-ykl
