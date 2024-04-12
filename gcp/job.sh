728bb76be8f030c0ab76026c538970289f9d5c01
ykl2023 <156436982+ykl2023@users.noreply.github.com>
2024-04-12 22:39:25 +0100

    Update job.sh (able to run )

--- gcp/job.sh
+++ gcp/job.sh
@@ -5,3 +5,22 @@ gcloud dataproc jobs submit pyspark \
   --py-files ../packages.zip \
   --files ../auto_arima.R,../darima_config.json \
   ../darima.py
+
+
+gcloud dataproc jobs submit pyspark PY_FILE (--cluster=cluster-st446-project-ykl)\
+   --bucket=st446-project-ykl-1204\
+  --project st446-19012024-ykl \  
+  --region europe-west2 \  
+  --py-files gs://st446-project-ykl-1204/student-dist-ARIMA-main/packages.zip \  
+  --files  gs://st446-project-ykl-1204/student-dist-ARIMA-main/auto_arima.R, gs://st446-project-ykl-1204/student-dist-ARIMA-main/darima_config.json \
+  gs://st446-project-ykl-1204/student-dist-ARIMA-main//darima.py
+  
+
+
+  gs://st446-project-ykl-1204/student-dist-ARIMA-main/auto_arima.R
+
+
+  st446-project-ykl-1204/student-dist-ARIMA-main/
+
+  gsutil ls gs://st446-project-ykl-1204/student-dist-ARIMA-main
+
