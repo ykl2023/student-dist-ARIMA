bad2639f1903b4c4f9f7f40710fdc5327ef8d583
ykl2023 <156436982+ykl2023@users.noreply.github.com>
2024-04-14 22:13:05 +0100

    Update install.sh

--- gcp/install.sh
+++ gcp/install.sh
@@ -3,3 +3,7 @@ apt-get install -y python-pip
 pip install pipenv
 pip install rpy2
 R -e "install.packages(c('forecast', 'polynom'), repos='https://cran.rstudio.com/')"
+
+R echo "install.packages('polynom', repos='http://cran.r-project.org')" | R --vanilla
+R echo "install.packages('forecast', repos='http://cran.r-project.org')" | R --vanilla
+
