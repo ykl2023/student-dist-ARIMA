2c19ed2f20e7b3c47e4c8ae43eebb7836a92ea6f
ykl2023 <156436982+ykl2023@users.noreply.github.com>
2024-04-14 19:46:04 +0100

    Update job.sh

--- gcp/job.sh
+++ gcp/job.sh
@@ -15,14 +15,14 @@ gcloud dataproc jobs submit pyspark PY_FILE (--cluster=cluster-st446-project-ykl
   --files  gs://st446-project-ykl-1204/student-dist-ARIMA/auto_arima.R, gs://st446-project-ykl-1204/student-dist-ARIMA-main/darima_config.json \
   gs://st446-project-ykl-1204/student-dist-ARIMA-main//darima.py
 
-
-  gcloud dataproc jobs submit pyspark gs://st446-project-ykl-1204/student-dist-ARIMA/darima.py \
-  --cluster=cluster-st446-project-ykl \
-  --bucket=st446-project-ykl-1204 \
+##### use this version, check bucket name and cluster name
+  gcloud dataproc jobs submit pyspark gs://cluster-st446-project-ykl/student-dist-ARIMA/darima.py \
+  --cluster=cluster-st446-project-ykl-1404 \
+  --bucket=bucket_st446_project_ykl \
   --project=st446-19012024-ykl \
   --region=europe-west2 \
-  --py-files=gs://st446-project-ykl-1204/student-dist-ARIMA/packages.zip \
-  --files=gs://st446-project-ykl-1204/student-dist-ARIMA/auto_arima.R,gs://st446-project-ykl-1204/student-dist-ARIMA/darima_config_updated.json
+  --py-files=gs://bucket_st446_project_ykl/student-dist-ARIMA/packages.zip \
+  --files=gs://bucket_st446_project_ykl/student-dist-ARIMA/auto_arima.R,gs://bucket_st446_project_ykl/student-dist-ARIMA/darima_config.json
 
 
 
